a1b9bc8b7005822b195dd69e0c4cb683d14a4ed8
Thomas Yi <thomasyi@cs.washington.edu>
2020-11-07 19:44:57 -0800

    preload

--- js/custom.js
+++ js/custom.js
@@ -16,7 +16,9 @@
     /*  IF YOU WANT TO USE DEVICE.JS TO DETECT THE VIEWPORT AND MANIPULATE THE OUTPUT  */
 
     //Device.js will check if it is Tablet or Mobile - http://matthewhudson.me/projects/device.js/
-    if (!device.tablet() && !device.mobile()) {} else {
+    if (!device.tablet() && !device.mobile()) {
+      // $(".player").mb_YTPlayer();
+    } else {
       //jQuery will add the default background to the preferred class
       $('.video-background').addClass(
         'video-background-default-image');
