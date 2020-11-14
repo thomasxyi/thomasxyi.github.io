9ea1e625920544279e036546172589795b3b7a51
Thomas Yi <thomasyi@cs.washington.edu>
2020-11-14 10:01:21 -0800

    js

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,7 @@
 
     //Device.js will check if it is Tablet or Mobile - http://matthewhudson.me/projects/device.js/
     if (!device.tablet() && !device.mobile()) {
-      $(".player").mb_YTPlayer();
+      $(".player").YTPlayer();
     } else {
       //jQuery will add the default background to the preferred class
       $('.video-background').addClass('video-background-default-image');
